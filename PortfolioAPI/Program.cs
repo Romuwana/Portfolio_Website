40cd565388d3c38d109d906df4a265da612652ff
Romuwana <nareromuwana@gmail.com>
2026-05-25 03:54:57 +0200

    Update Program.cs

--- PortfolioAPI/Program.cs
+++ PortfolioAPI/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddControllers();
 builder.Services.AddHttpClient();
 
 // Registers our custom AI logic
-//builder.Services.AddScoped<PortfolioAPI.Services.PortfolioAIService>();
+builder.Services.AddScoped<PortfolioAPI.Services.PortfolioAIService>();
 
 // 1. Use the native .NET 9 OpenAPI generator
 builder.Services.AddOpenApi();
